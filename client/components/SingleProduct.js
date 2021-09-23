c26724498cb7a0348deac87cea7c4a8af2e0d229
Peter Myer <peter.sr.myer@gmail.com>
2021-09-23 11:51:51 -0500

    created SingleProduct component

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -2,4 +2,31 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import {fetchStudent} from "../store/SingleStudent"
-import {Link} from 
+import {Link} from 'react-router=dom'
+import { render } from 'enzyme';
+
+
+class SingleProduct extends React.Component {
+  constructor() {
+    super ()
+    //eventually add a buy button
+  }
+
+
+  componentDigMount() {
+    this.props.getProduct(this.props.match.params.id, { history })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h2> {this.props.product.name} </h2>
+        <img src = {this.props.product.imageUrl}/>
+        <div>{this.props.product.price}</div>
+        <p>{this.props.product.description}</p>
+        <button>Add to Cart</button>
+      </div>
+    )
+  }
+}
