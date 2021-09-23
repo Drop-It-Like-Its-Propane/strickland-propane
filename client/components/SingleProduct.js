e9f7c480c0327989e4f3f908a36a5c65980c8294
Peter Myer <peter.sr.myer@gmail.com>
2021-09-23 11:42:35 -0500

    completed single product reducer

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,5 @@
+// REACT Single Product Component
+import React from 'react';
+import {connect} from 'react-redux'
+import {fetchStudent} from "../store/SingleStudent"
+import {Link} from 
---
+++ client/store/singleProduct.js
@@ -0,0 +1,44 @@
+//Single Product Reducer
+
+import axios from "axios";
+import { noExtendLeft } from "sequelize/types/lib/operators";
+
+//ACTION TYPES
+const SET_PRODUCT = "SET_PRODUCT";
+
+//ACTION CREATORS
+export const setProduct = (product) => {
+  return {
+    type: "SET_PRODUCT",
+    product,
+  };
+};
+
+//THUNK
+//retrieve single item from database using id
+export const fetchProduct = (id, history) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(`/api/products/${id}`);
+      dispatch(setProduct(response.data));
+    } catch (error) {
+      //this may need to be changed
+      history.push("/404");
+    }
+  };
+};
+
+//REDUCER
+//Initial State
+const initialState = {};
+
+//Reducer
+export default function productReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCT:
+      //add product to state
+      return Object.assign(...state, ...action.product);
+    default:
+      return state;
+  }
+}
