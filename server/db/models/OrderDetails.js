4c4a02d1aee15392d5c6e8385f8e349137db663a
Cassandra Wolff <cassandrawolff065@gmail.com>
2021-09-23 11:45:51 -0400

    merge conflict fixed

---
+++ server/db/models/OrderDetails.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const OrderDetails = db.define('orderDetails', {
+    orderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+            min: 0
+        }
+    },
+    totalPrice: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+            min: 0
+        }
+    }
+})
+
+const currencyAdjust = (details) => {
+    details.totalPrice = Math.floor(details.totalPrice * 100)
+  };
+
+  OrderDetails.beforeCreate(currencyAdjust);
+  OrderDetails.beforeBulkCreate((detail) => detail.map(currencyAdjust));
+
+module.exports = OrderDetails;
