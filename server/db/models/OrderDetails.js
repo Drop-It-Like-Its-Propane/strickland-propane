05b1141a1f2c36dc39248930783722743a8ee96f
Peter Myer <peter.sr.myer@gmail.com>
2021-09-24 14:22:45 -0500

    changed defaultValue on orderdetails  quantity

--- server/db/models/OrderDetails.js
+++ server/db/models/OrderDetails.js
@@ -13,7 +13,7 @@ const OrderDetails = db.define('orderDetails', {
     quantity: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        defaultValue: 0,
+        defaultValue: 1,
         validate: {
             min: 0
         }
