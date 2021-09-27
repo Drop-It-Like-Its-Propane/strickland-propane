dd7f34a0b1564319ccb0493f6f1c42d53dfb7801
Cassandra Wolff <cassandrawolff065@gmail.com>
2021-09-27 10:43:36 -0400

    button containers

--- client/components/Home.js
+++ client/components/Home.js
@@ -9,13 +9,13 @@ export const Home = props => {
   const {username} = props
 
   return (
-    <div>
-     <Link to="/products"> -All Products- </Link>
-     <Link to="/login"> -Login- </Link>
-     <Link to="/signup"> -Sign Up- </Link>
-     <a href="https://www.youtube.com/watch?v=9FA__4fLBos&t=48s">
-            -Learn More About Us-
-          </a>
+    <div className="container">
+     <div className="singleContainer"><Link to="/products"> All Products  </Link></div>
+     <div className="singleContainer"><Link to="/login"> Login  </Link></div>
+     <div className="singleContainer"><Link to="/signup"> Sign Up  </Link></div>
+     <div className="singleContainer"><a href="https://www.youtube.com/watch?v=9FA__4fLBos&t=48s">
+             Learn More About Us
+          </a></div>
     </div>
   )
 }
