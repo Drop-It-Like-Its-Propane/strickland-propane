ad0b62dd35d640be6c6f78920ed71ab18967922f
Peter Myer <peter.sr.myer@gmail.com>
2021-09-28 14:21:59 -0500

    bug fix: can add single item to cart again

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -6,7 +6,7 @@ import { addItem, fetchCart, createCart } from "../store/cart";
 import {me} from '../store/auth';
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css'; >This causes a crash, what does it do??
 
 //add 'Toast Notification" for adding item to cart
 //Add in STRIPE
@@ -19,15 +19,16 @@ class SingleProduct extends React.Component {
     this.insertDecimal = this.insertDecimal.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
+  //retrieve product and user, user User to get cart
   componentDidMount() {
     this.props.getUser()
     this.props.getProduct(this.props.match.params.id, { history });
   }
 
-  componentDidUpdate(prevProps){
-    if (!this.props.cart.orderId) {
-    this.props.getCart(this.props.user)}
-  }
+  // componentDidUpdate(prevProps){
+  //   if (!this.prevProps.cart !== this.props.cart) {
+  //   this.props.getCart(this.props.user)}
+  // }
 
   insertDecimal(num) {
     return (num / 100).toFixed(2);
@@ -35,7 +36,6 @@ class SingleProduct extends React.Component {
 
   handleClick(event) {
     event.preventDefault();
-    console.log(this.props.cart)
     if(!this.props.cart.id) {
       this.props.createCart(this.props.user, this.props.product)
     } else {
@@ -46,8 +46,8 @@ class SingleProduct extends React.Component {
     this.props.addItem(this.props.user, mergedDetails)}
   }
 
-  notify(){
-    return toast("Item added to cart!")}
+  // notify(){
+  //   return toast("Item added to cart!")}
 
   render() {
     const { product } = this.props;
@@ -57,8 +57,8 @@ class SingleProduct extends React.Component {
         <img className="singleProductImg" src="../proPAIN.jpg" />
         <div>{this.insertDecimal(product.price)}</div>
         <p>{product.description}</p>
-        <button onClick={this.handleClick, this.notify}>Add to Cart</button>
-        <ToastContainer />
+        <button onClick={this.handleClick}>Add to Cart</button>
+        {/* <ToastContainer /> */}
         <Link to="/products"> All Products </Link>
       </div>
     );
